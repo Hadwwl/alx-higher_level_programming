813f06e1f7627a067c78f18e4fa6abf6c36412c2
Hadwwl <ddjobs2021@gmail.com>
2023-12-07 09:38:12 -0800

    More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda sub_matrix: list(map(lambda i: i**2, sub_matrix)), matrix))
+    return list(map(lambda sub_matrix:
+                list(map(lambda i: i**2, sub_matrix)), matrix))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@ def roman_to_int(roman_string):
         return 0
     total = 0
     number = 0
-    dici_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dici_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                'C': 100, 'D': 500, 'M': 1000}
     for i in reversed(roman_string):
         number = dici_rom[i]
         total += number if total < number * 5 else -number
